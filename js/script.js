959211926f610300924cf3bd95f3beb7ea8c8680
jahbl <eliig24@hotmail.com>
2025-07-15 17:38:28 -0300

    inicializando

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function cambiarTexto() {
+console.log("cambiar texto");
+    document.getElementById("texto-magico").innerHTML = "¡Hola, mundo con JavaScript!";
+}
+
+document.getElementById("mi-boton")
+addEventListener("click", cambiarTexto);
+
+console.log("texto cambiado");
