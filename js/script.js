2e3ced29f6341b8d6e1c5fb3dd242f23c474fe0a
jahbl <eliig24@hotmail.com>
2025-07-22 18:35:08 -0300

    Segundo commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ console.log("cambiar texto");
     document.getElementById("texto-magico").innerHTML = "¡Hola, mundo con JavaScript!";
 }
 
-document.getElementById("mi-boton")
-addEventListener("click", cambiarTexto);
+document.getElementById("mi-boton").addEventListener("click", cambiarTexto);
 
 console.log("texto cambiado");
